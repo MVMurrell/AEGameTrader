9a19be4bac7de56f4d4b23ae42646c5aebd18440
Michael <mmurrell@bbgres.com>
2020-02-21 03:18:41 -0700

    aeternit project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreFrontComponent } from './store-front/store-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreFrontComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
